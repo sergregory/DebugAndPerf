95d70f27d8197dc95cb04606945bffcfd5ceb259
dbobkova <das-bobkova@yandex.ru>
2016-01-22 17:55:10 +0400

    ADD library #include <time.h> and function clock()

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include "perf.hpp"
+#include <iostream>
+#include <time.h>
+using namespace std;
 
 int main()
 {
-	
+
+	int a =4;
+	long t1 = clock();
+	for(int i = 0; i < 3; ++i)
+	{
+		
+		a = a + 2;
+		
+
+	}
+	long t2 = clock();
+	cout << t2 - t1 << endl;
     return 0;
 }
