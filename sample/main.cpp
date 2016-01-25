6c3d8bc21c7f1d958b9cc7de17c32066b06c5c9f
dbobkova <das-bobkova@yandex.ru>
2016-01-25 19:21:41 +0400

    ADD functions calcTicTime and calcTocTime (v.2)

--- sample/main.cpp
+++ sample/main.cpp
@@ -1,6 +1,15 @@
 #include "perf.hpp"
 
+
 int main()
 {
+	int a =4;
+	TIC(loop);
+	for(int i = 0; i < 9000000; ++i)
+	{
+		a = a + 2;
+	}
+	TOC(loop);
+	
     return 0;
 }
--- src/perf.hpp
+++ src/perf.hpp
@@ -1,5 +1,8 @@
 #include <ctime>
 #include <iostream>
 
+#define TIC(name) clock_t start_##name = calcTicTime(); 
+#define TOC(name) calcTocTime(start_##name);
+
 clock_t calcTicTime();
 void calcTocTime(clock_t ticTime);
