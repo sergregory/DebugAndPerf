82ab26622871760a4cbf57eb92ceea115a2edac9
dbobkova <das-bobkova@yandex.ru>
2016-01-25 15:06:31 +0400

    Add macros #define

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 #include <ctime>
 using namespace std;
 
+#define FUNCTION(id,name) void _##id() {clock_t t1 = clock(); cout <<#name << float(t1)/ CLOCKS_PER_SEC << " sec "<< endl;};
+FUNCTION (TIC,loop)
+FUNCTION (TOC,loop)
+	
+
+
 int main()
 {
 	int a =4;
-	clock_t t1 = clock();
-	for(int i = 0; i < 900000; ++i)
+	//clock_t t1 = clock();
+	_TIC();
+	for(int i = 0; i < 9000000; ++i)
 	{
 		a = a + 2;
 	}
-	clock_t t2 = clock();
-	cout << float(t2 - t1)/ CLOCKS_PER_SEC << " sec "<< endl;
+	_TOC();
+	
+	//clock_t t2 = clock();
+	//cout << float(t2 - t1)/ CLOCKS_PER_SEC << " sec "<< endl;
     return 0;
 }
