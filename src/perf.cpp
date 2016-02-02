fea2f2e019f10372d72e01d584f55653645bc125
dbobkova <das-bobkova@yandex.ru>
2016-02-02 18:47:23 +0400

    Edit class Statistics (v.6) - change function

--- src/perf.cpp
+++ src/perf.cpp
@@ -9,11 +9,7 @@ void calcTocTime(perf_time_t ticTime){
 Statistics timeStatistics;
 
 void startStatistics(){
-	std::cout<<"Ncalls: "<<timeStatistics.getNcalls()<<std::endl;
-	std::cout<<"totalT: "<<timeStatistics.getTotalT()<<std::endl;
-	std::cout<<"averageT: "<<timeStatistics.getAverageT()<<std::endl;
-	std::cout<<"isStarted: "<<timeStatistics.getIsStarted()<<std::endl;
-	std::cout<<std::endl;
+	
 }
 void finishStatistics(){
 	timeStatistics.setAverageT();
@@ -29,10 +25,17 @@ void finishStatistics(){
 perf_time_t calcTimeStatistics(std::string name){
 	
 	if (timeStatistics.getIsStarted()==0)
-	{
+	{	
+		
 		timeStatistics.setStasticsName(name);
 		timeStatistics.setNewStatistic();
 	    timeStatistics.changeIsStartedTrue();
+
+		std::cout<<"Ncalls: "<<timeStatistics.getNcalls()<<std::endl;
+		std::cout<<"totalT: "<<timeStatistics.getTotalT()<<std::endl;
+		std::cout<<"averageT: "<<timeStatistics.getAverageT()<<std::endl;
+		std::cout<<"isStarted: "<<timeStatistics.getIsStarted()<<std::endl;
+		std::cout<<std::endl;
 	}
 	printStatisticName();
 	return clock();
